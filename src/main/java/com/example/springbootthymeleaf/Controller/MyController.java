b131b3dd588bdc9c3ee8475c114d848dfefccf66
Tamzid Israk <tamzidisrak12@gmail.com>
2023-07-10 03:27:58 +0600

    Server Side validation

--- src/main/java/com/example/springbootthymeleaf/Controller/MyController.java
+++ src/main/java/com/example/springbootthymeleaf/Controller/MyController.java
@@ -2,9 +2,16 @@ package com.example.springbootthymeleaf.Controller;
 
 import java.util.List;
 
+import com.example.springbootthymeleaf.Modal.LoginData;
+
+import jakarta.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 //RequestMapping is only for html file + we can also set method inside it
@@ -46,6 +53,23 @@ public class MyController {
     @GetMapping("/contact")
     public String newBase(){
         return "contact";
+    }
+
+    @GetMapping("/form")
+    public String loginForm(Model model){
+        model.addAttribute("loginData",new LoginData());
+        return "form";
+    }
+
+
+    @PostMapping("/process")
+    public String process(@Valid @ModelAttribute("loginData") LoginData loginData,BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            System.out.println(bindingResult);
+            return "form";
+        }
+        System.out.println(loginData);
+        return "sucess";
     }
      
     
--- src/main/java/com/example/springbootthymeleaf/Modal/LoginData.java
+++ src/main/java/com/example/springbootthymeleaf/Modal/LoginData.java
@@ -1,2 +1,59 @@
-package com.example.springbootthymeleaf.Modal;public class LoginData {
+package com.example.springbootthymeleaf.Modal;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class LoginData {
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3,max = 12,message = "username must from 3 to 12 character")
+    private String userName;
+
+    @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message = "invalid email")
+    private String email;
+
+    @AssertTrue
+    private boolean agreed;
+    
+
+    public LoginData(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public LoginData() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginData [userName=" + userName + ", email=" + email + "]";
+    }
+
+    public boolean isAgreed() {
+        return agreed;
+    }
+
+    public void setAgreed(boolean agreed) {
+        this.agreed = agreed;
+    }
+
+
 }
